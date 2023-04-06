792512d25ea7eaea6f8c5f23b765db58a1db9ee9
Daniel Jaimes <daniel.jaimes-carrillo@capgemini.com>
2023-04-06 09:46:41 +0200

    Batch

---
+++ src/main/java/com/example/batch/PeopleBatchConfiguration.java
@@ -0,0 +1,75 @@
+package com.example.batch;
+
+import com.example.model.People;
+import com.example.model.PeopleDTO;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PeopleBatchConfiguration {
+	@Autowired
+	JobRepository jobRepository;
+	@Autowired
+	PlatformTransactionManager transactionManager;
+	
+	public FlatFileItemReader<PeopleDTO> peopleCSVItemReader(String fname) {
+		return new FlatFileItemReaderBuilder<PeopleDTO>().name("personaCSVItemReader")
+				.resource(new ClassPathResource(fname))
+				.linesToSkip(1)
+				.delimited()
+				.names("id", "nombre", "apellidos", "correo", "sexo", "ip")
+				.fieldSetMapper(new BeanWrapperFieldSetMapper<PeopleDTO>() { {
+						setTargetType(PeopleDTO.class);
+					}})
+				.build();
+	}
+	@Autowired
+	public PeopleItemProcessor peopleItemProcessor;
+
+	@Bean
+	public JdbcBatchItemWriter<People> personaDBItemWriter(DataSource dataSource) {
+		return new JdbcBatchItemWriterBuilder<People>()
+				.itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+				.sql("INSERT INTO personas VALUES (:id,:nombre,:correo,:ip)")
+				.dataSource(dataSource)
+				.build();
+	}
+	
+	@Bean
+	public Step importCSV2DBStep1(JdbcBatchItemWriter<People> personaDBItemWriter) {
+		return new StepBuilder("importCSV2DBStep1", jobRepository)
+				.<PeopleDTO, People>chunk(10, transactionManager)
+				.reader(peopleCSVItemReader("personas-1.csv"))
+				.processor(peopleItemProcessor)
+				.writer(personaDBItemWriter)
+				.build();
+	}
+	
+	@Bean
+	public Job personasJob(PeopleJobListener listener, Step importCSV2DBStep1) {
+		return new JobBuilder("personasJob", jobRepository)
+				.incrementer(new RunIdIncrementer())
+				.listener(listener)
+				.start(importCSV2DBStep1)
+				.build();
+	}
+
+
+}
---
+++ src/main/java/com/example/batch/PeopleItemProcessor.java
@@ -0,0 +1,22 @@
+package com.example.batch;
+
+import com.example.model.People;
+import com.example.model.PeopleDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PeopleItemProcessor implements ItemProcessor<PeopleDTO, People> {
+	private static final Logger log = LoggerFactory.getLogger(PeopleItemProcessor.class);
+	
+	@Override
+	public People process(PeopleDTO item) throws Exception {	 
+		if(item.getId() % 2 == 0 || "Male".equals(item.getSexo())) return null;
+		People rslt = new People(item.getId(), item.getApellidos() + ", " + item.getNombre(), 
+				item.getCorreo(), item.getIp());
+		log.info("Procesando: " + item);
+		return rslt;
+	}
+}
